689e06ca6078fcd72f2e2a03d3d8ba9839edd257
destaze <thgoe68@gmail.com>
2025-10-28 13:20:53 +0300

    Add files via upload

---
+++ ПР3 Поляков.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    int num1;
+    int num2;
+    int result;
+
+   
+    cout << "Таблица умножения:" << endl << endl;
+
+    for (num1 = 1; num1 <= 9; num1++)
+    {
+        for (num2 = 1; num2 <= 9; num2++)
+        {
+            result = num1 * num2;
+            cout << result << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+    
+    
+    int num;
+    int num_now;
+
+    cout << "Поиск делителя. Введите число: ";
+    cin >> num;
+
+    cout << "Делители числа " << num << ": ";
+
+    for (num_now = 1; num_now <= num; num_now = num_now + 1)
+    {
+        if (num % num_now == 0)
+        {
+            cout << num_now << " ";
+        }
+    }
+
+    cout << endl;
+
+    return 0;
+}
